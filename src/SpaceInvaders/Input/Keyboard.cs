41440cd8dd796a3281c251aabb9c9ef056ecef03
Hato1125 <hato.flavius@gmail.com>
2023-06-17 22:33:45 +0900

    キーボードの入力クラスを作成

---
+++ src/SpaceInvaders/Input/Keyboard.cs
@@ -0,0 +1,33 @@
+using System.Runtime.InteropServices;
+using SDL2;
+
+namespace SpaceInvaders.Input;
+
+internal static class Keyboard
+{
+    private static readonly byte[] keyState = new byte[512];
+    private static readonly byte[] keyValue = new byte[512];
+
+    public static void Update()
+    {
+        var state = SDL.SDL_GetKeyboardState(out int length);
+        Marshal.Copy(state, keyState, 0, length);
+
+        for(int i = 0; i < keyState.Length; i++)
+        {
+            if(keyState[i] == 1)
+                keyValue[i] = (byte)(IsPushing((SDL.SDL_Scancode)i) ? 2 : 1);
+            else
+                keyValue[i] = (byte)(IsPushing((SDL.SDL_Scancode)i) ? 3 : 0);
+        }
+    }
+
+    public static bool IsPushing(SDL.SDL_Scancode keyCode)
+        => keyValue[(int)keyCode] > 0;
+
+    public static bool IsPushed(SDL.SDL_Scancode keyCode)
+        => keyValue[(int)keyCode] == 1;
+
+    public static bool IsSeparate(SDL.SDL_Scancode keyCode)
+        => keyValue[(int)keyCode] == 3;
+}
