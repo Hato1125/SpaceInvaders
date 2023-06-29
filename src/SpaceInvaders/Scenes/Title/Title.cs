797fcc6102375911f09d9cc37dd42c3a3f7b97c2
Hato1125 <hato.flavius@gmail.com>
2023-06-29 20:08:05 +0900

    CharaPointの表示

--- src/SpaceInvaders/Scenes/Title/Title.cs
+++ src/SpaceInvaders/Scenes/Title/Title.cs
@@ -8,20 +8,24 @@ namespace SpaceInvaders.Scenes.Title;
 internal class Title : Scene
 {
     private Sprite? titleSprite;
+    private Sprite? charaSprite;
 
     public override void Init()
     {
         RoundScene.RoundCount = 1;
         titleSprite = new(App.App.Window.RendererPtr, $"{AppInfo.TitleTextureDire}Title.png");
+        charaSprite = new(App.App.Window.RendererPtr, $"{AppInfo.TitleTextureDire}CharaPoint.png");
     }
 
     public override void Render()
     {
         titleSprite?.Render((AppInfo.Width - titleSprite.Width) / 2, 100);
+        charaSprite?.Render((AppInfo.Width - charaSprite.Width) / 2, 425);
     }
 
     public override void Finish()
     {
         titleSprite?.Dispose();
+        charaSprite?.Dispose();
     }
 }
